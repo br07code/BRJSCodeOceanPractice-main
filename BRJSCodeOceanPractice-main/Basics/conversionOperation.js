071c9b378af41667b136c054f6e3b5088138f9df
br07code <balaram.7872roy@gmail.com>
2024-01-27 03:58:10 +0530

    Conversion of datatypes

---
+++ BRJSCodeOceanPractice-main/Basics/conversionOperation.js
@@ -0,0 +1,48 @@
+"use strict"
+let score = "87"
+let Score = "233bhbj"
+
+/*
+console.log(typeof score) //string
+console.log(typeof (score)) //string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) //number
+
+let ValueInSting = Number(Score) 
+console.log(ValueInSting) //NaN
+*/
+
+//Notes:
+// "87" => 87
+// "233bhbj" => NaN
+// true => 1
+// false => 0
+
+
+/*
+let isLoggedIn = true
+
+//let booleanIsLoggedIn = Number(isLoggedIn)
+console.log(booleanIsLoggedIn) // 1
+
+*/
+
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true
+*/
+
+let name = "Balaram"
+
+let isStringNumber = Number(name)
+console.log(isStringNumber) //NaN
+
+
+let num = 1233
+
+let isNumString = String(num)
+console.log(isNumString) // 1233
+console.log(typeof isNumString); //string
