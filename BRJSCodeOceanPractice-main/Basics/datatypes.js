a39bfc3c4d8873ef5c0e99bea446fd03e51b643b
br07code <balaram.7872roy@gmail.com>
2024-01-27 02:22:07 +0530

    Push from vscode

---
+++ BRJSCodeOceanPractice-main/Basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all javascript code as newer version
+
+ //alert(3 + 3) //throw error because of we are using nodejs, not browser
+
+ let name = "Balaram"
+ let age = 26
+ let isLoggedIn = true
+ let state = null
+
+ //number => 2 to power 53
+ //bigint => storing huge value
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+//symbol => unique
+//object
+
+console.log(typeof "balaram");
---
+++ BRJSCodeOceanPractice-main/Basics/variables.js
@@ -0,0 +1,25 @@
+const accountId = 12345
+let accountName = "BalaramRoy_Acc"
+let accountEmail = "balaram.7872roy@gmail.com"
+var accountPassword = "1234t53"
+accountCity = "Pune"
+let accountState // it wil print "undefined"
+
+accontID = 4564746 //const value can't be changed
+
+accountEmail = "bala2353@gmail.com" //allowed
+
+accountPassword = "665fhbhj" // allowed
+
+accountState = "MH"
+
+/*
+Note:
+Prefer not to use "var"
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId); // op - 12345
+
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity, accountState])
+ 
