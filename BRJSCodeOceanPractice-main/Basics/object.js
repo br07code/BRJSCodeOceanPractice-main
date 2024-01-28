60d301cde0c7ced3e047f93fa2d6cd151a574647
br07code <balaram.7872roy@gmail.com>
2024-01-29 04:07:57 +0530

    Add different types creation of Objects and accessing the properties of it

--- BRJSCodeOceanPractice-main/Basics/object.js
+++ BRJSCodeOceanPractice-main/Basics/object.js
@@ -5,25 +5,27 @@
 // Define an object constructor, and then create objects of the constructed type.
 // Create an object using Object.create().
 
-const mySymbol = Symbol(`key1`)
+// const mySymbol = Symbol(`key1`)
 
-// Using Object literals
-// Example--1
-// Spaces and line breaks are not important. An object definition can span multiple lines:
-const myObject1 = {
-    firstName: `Balaram`,
-    "full name": `Balaram Roy`,
-    // mySymbol:  `myKey1`, // This line will not treat as a symbol tyoe will be string
-    // If we want to use it for symbol then we need to enclose it with "[]" (i.e. [mySymbol])
-    [mySymbol]: `mykey1`,
-    location: `Pune`,
-    age: 26,
-    email: `balaram.7872roy@gmail.com`,
-    isLoggedIn: true,
-    logInDays: [`Monday`, `Tuesday`, `Wednesday`, `Thusday`, `Friday`]
-}
+// ************************ Method-1 : Using Object literals *****************************//
 
-console.log(myObject1);
+// // *********Example-1***************//
+// // Spaces and line breaks are not important. An object definition can span multiple lines:
+
+// const myObject1 = {
+//     firstName: `Balaram`,
+//     "full name": `Balaram Roy`,
+//     // mySymbol:  `myKey1`, // This line will not treat as a symbol tyoe will be string
+//     // If we want to use it for symbol then we need to enclose it with "[]" (i.e. [mySymbol])
+//     [mySymbol]: `mykey1`,
+//     location: `Pune`,
+//     age: 26,
+//     email: `balaram.7872roy@gmail.com`,
+//     isLoggedIn: true,
+//     logInDays: [`Monday`, `Tuesday`, `Wednesday`, `Thusday`, `Friday`]
+// }
+
+// console.log(myObject1);
 
 
 // Displaying the element of an object
@@ -38,64 +40,76 @@ console.log(myObject1);
 // console.log(myObject1.firstName, myObject1.location, myObject1.age, myObject1.email, myObject1.isLoggedIn, myObject1.logInDays);
 
 // Changing value of a properties of an Object
-myObject1.age = 27
-console.log(myObject1.age);
-console.log(myObject1);
+// myObject1.age = 27
+// console.log(myObject1.age);
+// console.log(myObject1);
 
-// Freezing Object 
-Object.freeze(myObject1)
-myObject1.age = 91
-console.log(myObject1);
+// // Freezing Object 
+// Object.freeze(myObject1)
+// myObject1.age = 91
+// console.log(myObject1);
 
 
-// // Example--2 
-// //This example creates an empty JavaScript object, and then adds 4 properties:
+// ****************** Example-2 ************************//
+//This example creates an empty JavaScript object, and then adds 4 properties:
+
 // const myObject2 = {}
 // myObject2.firstName = "Balaram"
 // myObject2.lastName = "Roy"
 // myObject2.age = 26
 // myObject2.email = "balandib@gmail.com"
+// // console.log(myObject2);
+// console.log(myObject2.firstName, myObject2.lastName, myObject2.age, myObject2.email);
 
 
-// // Using new keyword
-// // The following example create a new JavaScript object using new Object(), and then adds 4 properties:
+//*********************************** Method-2 : Using new keyword ********************************//
+
+// The following example create a new JavaScript object using new Object(), and then adds 4 properties:
 // const myObject3 = new Object()
-// myObject3.favBook = ""
+// myObject3.favBook1 = "Physics"
+// myObject3.favBook2 = "Chemistry"
+// myObject3.favBook3 = "Math"
+// myObject3.favNum1 = 7
+// myObject3.favNum2 = 8
+// myObject3.favNum3 = 9
+
+// console.log(myObject3);
 
 
-// // Using Object.create()
-// let Animal = {
-//     isHuman: false,
-//     sound: "Unspecified",
-//     makeSound() {
-//       console.log(this.sound);
-//     },
-//   };
+// ****************************** Method-3 : Using Object.create() ************************************//
+
+let Animal = {
+    isHuman: false,
+    sound: "Unspecified",
+    makeSound() {
+      console.log(this.sound);
+    },
+  };
   
-//   // create object from Animal prototype
-//   let snake = Object.create(Animal);
-//   snake.makeSound(); // Unspecified
+  // create object from Animal prototype
+  let snake = Object.create(Animal);
+  snake.makeSound(); // Unspecified
   
-//   // properties can be created  and overridden
-//   snake.sound = "Hiss";
-//   snake.makeSound(); // Hiss
+  // properties can be created  and overridden
+  snake.sound = "Hiss";
+  snake.makeSound(); // Hiss
   
-//   // can also directly initialize object properties with second argument
-//   let properties = {
-//     isHuman: {
-//       value: true,
-//     },
-//     name: {
-//       value: "Jack",
-//       enumerable: true,
-//       writable: true,
-//     },
-//     introduce: {
-//       value: function () {
-//         console.log(`Hey! I am ${this.name}.`);
-//       },
-//     },
-//   };
+  // can also directly initialize object properties with second argument
+  let properties = {
+    isHuman: {
+      value: true,
+    },
+    name: {
+      value: "Jack",
+      enumerable: true,
+      writable: true,
+    },
+    introduce: {
+      value: function () {
+        console.log(`Hey! I am ${this.name}, "Is Jack human?" ${this.isHuman}`);
+      },
+    },
+  };
   
-//   human = Object.create(Animal, properties);
-//   human.introduce(); // Hey! I am Jack.
+  human = Object.create(Animal, properties);
+  human.introduce(); // ey! I am Jack, "Is Jack human?" true
